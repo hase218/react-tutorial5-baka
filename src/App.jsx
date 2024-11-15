b677f0968bf30277c21b06450961c231d40ee7b1
hase218 <chju23041@g.nihon-u.ac.jp>
2024-11-16 00:48:51 +0900

    変数makeStoryを作成して、ボタンが押されるたびにバカ話を読み込みなおした

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 
 export default function App() {
-
+  const [story, setStory] = useState(""); //読み込んだ内容入ってる
   const [showStory, setShowStory] = useState(false); //ボタンを押したらバカ話を見せる
+  //最初の一回だけなのにuseStateにする意味あんのか？
+
+  const [makeStory, setMakeStory] = useState("false");
   
   const [name, setName] = useState("");
 
-  const [story, setStory] = useState("");
   const [ukus, setUkUs] = useState("us");
   const [units, setUnits] = useState(["94 fahrenheit", "300pounds"]);
 
@@ -14,17 +16,23 @@ export default function App() {
     (async () => {
       const response = await fetch("/.netlify/functions/bakaBanashi");
       const data = await response.json();
-      console.log(data)
+      console.log(data);
+      console.log(makeStory);
       setStory(data);
     })();
-  }, [setShowStory]);
+  }, [makeStory]);
 
-  //useEffect、第 1 引数には、副作用を起こす関数、第 2 引数には、その副作用が依存する値のリストを配列で渡す
-  //下のhandleClicke関数内にぶち込むよりもなにかを第二引数にした方がいいんだろうなぁ...?
+  //useEffect、第1引数には、副作用を起こす関数、第2引数には、その副作用が依存する値のリストを配列で渡す
+
   //目標、ボタンが押すたびにランダムな話がでてくる
+  //下のhandleClicke関数内にぶち込むよりもなにかを第二引数にした方がいいんだろうな、なにを引数にすれば？？？
+  //あたらしくそれ用の作らなきゃだめ・・・ってコト！？いったんそれでやってみるか
+  //makeStoryつくってみた、変数多くてキモ
+
 
   function handleClickResetButton() {
     setShowStory(true);
+    makeStory === true ? setMakeStory(false) : setMakeStory(true);
   }
   
   return (
